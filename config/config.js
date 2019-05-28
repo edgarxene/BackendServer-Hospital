516be7ed4602b7e724cbb37de9cc3df5262ff4f6
Edgar Cruz <edgarxene@gmail.com>
2019-05-28 13:57:20 -0500

    Seccion 12 Terminada

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '397955015548-rkr4t7v81nhbhmlv7tovhrpglu6k2u66.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'ulpo2o3oMkffIacw455hexyl';
